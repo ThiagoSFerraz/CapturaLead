12e00c9a8b7907a1a1d3ada0a9a2df12dcbb5335
ThiagoSFerraz <oniferraz@gmail.com>
2021-05-24 14:08:50 -0300

    pequenas alterações

--- src/components/BenefitsSlider/BenefitsSlider.jsx
+++ src/components/BenefitsSlider/BenefitsSlider.jsx
@@ -1,19 +1,17 @@
-
 import BenefitsSliderContent from "./BenefitsSliderContent";
 import Dots from "./Dots";
 import { useState } from "react";
-import '../../styles/components/benefitsSlider.css'
-
+import "../../styles/components/benefitsSlider.css";
 
 export default function BenefitsSlider() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <div className="sliderContainer">      
-      <BenefitsSliderContent activeIndex={activeIndex} />     
+    <div className="sliderContainer">
+      <BenefitsSliderContent activeIndex={activeIndex} />
       <Dots
-      activeIndex={activeIndex}
-      onClick={(activeIndex) => setActiveIndex(activeIndex)}
+        activeIndex={activeIndex}
+        onClick={(activeIndex) => setActiveIndex(activeIndex)}
       />
     </div>
   );
--- src/components/ClientSlider/ClientSlider.jsx
+++ src/components/ClientSlider/ClientSlider.jsx
@@ -1,35 +1,40 @@
-import Slider from 'react-slick'
-import image from './images/example.png'
-import '../../styles/components/clientSlider.css'
-import { useState } from 'react';
+import Slider from "react-slick";
+import image from "./images/example.png";
+import "../../styles/components/clientSlider.css";
+import { useState } from "react";
 
-const images = [image, image, image, image, image, image, image]
+const images = [image, image, image, image, image, image, image];
 
-export default function ClientSlider(){
+export default function ClientSlider() {
+  const [imageIndex, setImageIndex] = useState(0);
+  const settings = {
+    className: "center",
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    focusOnSelect: true,
+    centerMode: true,
+    centerPadding: 0,
+    dots: true,
+    dotsClass: "slick-dots",
+    beforeChange: (current, next) => setImageIndex(next),
+  };
 
-    const [imageIndex, setImageIndex] = useState(0);
-    const settings = {
-        className:'center',
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        focusOnSelect: true,
-        centerMode: true,
-        centerPadding: 0,
-        dots: true,
-        dotsClass: 'slick-dots',
-        beforeChange: (current, next) => setImageIndex(next),
-    }
-
-    return(
-        <div id="clientSlider">
-            <Slider {...settings}>
-                {images.map((img, idx) => (
-                    <div className={idx === imageIndex ? "clientSlide activeClientSlide" : "clientSlide"}>
-                        <img className="image" src={img} alt="" />
-                    </div>
-                ))}
-            </Slider>
-        </div>
-    )
+  return (
+    <div id="clientSlider">
+      <Slider {...settings}>
+        {images.map((img, idx) => (
+          <div
+            className={
+              idx === imageIndex
+                ? "clientSlide activeClientSlide"
+                : "clientSlide"
+            }
+          >
+            <img className="image" src={img} alt="" />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
